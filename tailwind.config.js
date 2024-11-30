1d6217f68b47a21487e9ea77aedf62b9284ff17b
NastroCoding <naufalaqilnasrullah0502@gmail.com>
2024-11-30 12:46:32 +0700

    done?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    'index.html',
-    './pages/**/*.html'
-  ],
+  content: ["index.html", "./pages/**/*.html"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        "primary-dark": "#1a202c",
+        "primary-light": "#f7fafc",
+        "secondary-dark": "#2d3748",
+        "secondary-light": "#edf2f7",
+      },
+      boxShadow: {
+        card: "0 10px 20px rgba(0, 0, 0, 0.1)",
+        "card-dark": "0 10px 20px rgba(99, 102, 241, 0.3)",
+      },
+    },
   },
-  plugins: [require('flowbite/plugin')],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
